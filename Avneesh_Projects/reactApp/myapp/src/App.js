9a6d0a6d4fc7bea164f1c8a5612aefb31be2733a
Avneesh <avneeshdwivedi.tft@gmail.com>
2019-09-19 10:37:05 +0530

    second commit

--- Avneesh_Projects/reactApp/myapp/src/App.js
+++ Avneesh_Projects/reactApp/myapp/src/App.js
@@ -1,39 +1,73 @@
 import React,{Component,Fragment} from 'react';
-//import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router,NavLink,Link} from "react-router-dom";
+import {BrowserRouter as Router,NavLink} from "react-router-dom";
 import Route from "react-router-dom/Route";
-
-//import Facebook from './Facebook';
+import Registration from './Registration';
+import Login from './Login';
 
 class App extends Component{
-  state={
-    loggedIn:false
-  }
-  clickhandler = () =>{
-this.setState={
-  loggedIn:true
-}
-  }
-render(){
-  return (
- <Router>
-<div className="App">
-  <NavLink to="/about" >
-    </NavLink>
-    <input type="button" value="Log In" onClick={this.clickhandler} />
-<Route path="/" exact strict
-render={
-  ()=>{
-    return (<h1>Welcome to my first react App</h1>)
-  }
-}
-  />
 
-</div>
-</Router>
-  );
-}
+  render() {
+    return (
+      <Router>
+        <div className="form-wrapper">
+        <ul>
+          <li>
+            <NavLink to="/" exact  >Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" exact  >login</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/register" exact strict >register</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/user" exact strict>user</NavLink>
+          </li>
+          </ul>
+          {/* <Prompt
+            when={!this.state.loggedIn}
+            message={(location)=>{
+               return location.pathname.startsWith('/user') ? 'Are you sure?' : true
+             }}
+          /> */}
+
+        <Route path="/" exact strict render={
+          () => {
+            return (
+<h1 className="form-wrapper">Hi Welcome to App.js File thnk you visiting</h1>
+            );
+          }
+        }/>
+                <Route path="/login" exact strict render={
+          () => {
+            return (
+<Login />
+            );
+          }
+        }/>
+                <Route path="/register" exact strict render={
+          () => {
+            return (
+<Registration />
+            );
+          }
+        }/>
+                <Route path="/user" exact strict render={
+          () => {
+            return (
+<p>Welcome to my user web page</p>
+            );
+          }
+        }/>
+       
+        </div>
+      </Router>
+    );
+  }
+
 }
 
 export default App;
--- Avneesh_Projects/reactApp/myapp/src/Registration.js
+++ Avneesh_Projects/reactApp/myapp/src/Registration.js
@@ -1,1 +1,79 @@
-import React from "react";
+import React,{Component,Fragment} from "react";
+import './App.css';
+
+class Registration extends Component{
+constructor(props){
+this.state={
+    first:null,
+    middle:null,
+    last:null,
+    age:null,
+    email:null,
+    contact:null,
+    password:null,
+    nationality:null
+}
+submithandler=()=>{
+this.setState(arg=>({
+    first:arg.first,
+    middle:arg.middle,
+    last:arg.last,
+    age:arg.age,
+    email:arg.email,
+    contact:arg.contact,
+    password:arg.password,
+    nationality:arg.nationality
+}))
+}
+}
+
+    render(){
+return (
+<Fragment>
+<div class="form-wrapper">
+  <form onSubmit={submithandler}>
+    <h3>Login here</h3>
+
+    <div class="form-item">
+                <input type="text"  value={this.state.first} required />
+    </div>
+    <div class="form-item">
+                <input type="text"   value={this.state.middle} required />
+    </div>
+    <div class="form-item">
+                <input type="text"   value={this.state.last} required />
+    </div>
+    <div class="form-item">
+                <input type="text"   value={this.state.age} required />
+    </div>
+    <div class="form-item">
+                <input type="text"   value={this.state.email} required />
+    </div>
+    <div class="form-item">
+                <input type="text"   value={this.state.contact} required />
+    </div>
+    <div class="form-item">
+                <input type="text"   value={this.state.nationality} required />
+    </div>
+    <div class="form-item">
+                <input type="password" value={this.state.password} required />
+    </div>
+    
+    <div class="button-panel">
+                <input type="submit" class="button" title="Log In" />
+    </div>
+  </form>
+  <div class="reminder">
+    <p>Already have an Account <a href={}>Login Now</a></p>
+  </div>
+  
+</div>
+
+</Fragment>
+
+);
+
+}
+
+}
+export default Registration;
